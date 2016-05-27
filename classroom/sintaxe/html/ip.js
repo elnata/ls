6c719d7656a411b5f368fe3898762deda99435e9
Luiz Carlos <lucachaves@gmail.com>
2016-05-27 11:36:03 -0300

    calculadora e table de ips no HTML

---
+++ classroom/sintaxe/html/ip.js
@@ -0,0 +1,16 @@
+var ip = {
+  "as":"AS15169 Google Inc.",
+  "city":"Mount'ain View",
+  "country":"United States",
+  "countryCode":"US",
+  "isp":"Google",
+  "lat":37.386,
+  "lon":-122.0838,
+  "org":"Google",
+  "query":"8.8.8.8",
+  "region":"CA",
+  "regionName":"California",
+  "status":"success",
+  "timezone":"America/Los_Angeles",
+  "zip":"94035"
+};
